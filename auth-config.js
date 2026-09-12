5979ea4d4e59c277d49b7b14d7a7d69f14e046f6
turkuserz <darkkiss0099@gmail.com>
2026-09-12 18:24:15 +0700

    Add files via upload

--- auth-config.js
+++ auth-config.js
@@ -1,2 +1,3 @@
-// Put the address of your TURK authentication worker here.
+// TURK authentication endpoint
+// Put your deployed Cloudflare Worker URL here.
 window.TURK_AUTH_URL = 'https://YOUR-WORKER.workers.dev/verify';
